4dfc1278dedc37f2360ff15cc060211d87dcb142
DylanDePasquale24 <86976398+DylanDePasquale24@users.noreply.github.com>
2024-09-29 23:06:58 -0400

    Before adding Chakra

--- Frontend/app/src/App.js
+++ Frontend/app/src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 import Login from "./Pages/Authentication/Login";
 import Register from "./Pages/Authentication/Register";
 import Dashboard from "./Pages/HomePage/HomePageComponents/Dashboard";
-import Applications from "./Pages/MyApplications/Applications";
+import Applications from "./Pages/MyApplications/ApplicationsPage";
 import Resumes from "./Pages/HomePage/HomePageComponents/Resumes";
 import Analytics from "./Pages/HomePage/HomePageComponents/Analytics";
 import Settings from "./Pages/HomePage/HomePageComponents/Settings";
