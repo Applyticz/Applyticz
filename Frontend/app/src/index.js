645cf41fbfe8274cacee0a09b41e351f67a1d9bc
DylanDePasquale24 <86976398+DylanDePasquale24@users.noreply.github.com>
2024-09-29 23:54:14 -0400

    Removed Chakra for now

--- Frontend/app/src/index.js
+++ Frontend/app/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { AuthProvider } from "./authContext";
-import {ChakraProvider} from '@chakra-ui/react'
+
 
 const root = ReactDOM.createRoot(document.getElementById("root")); // Create a root.
 
